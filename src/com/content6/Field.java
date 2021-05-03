c53903cead578bd0b603bd4f16ea916199256532
Sinitsa01 <sinitsa2001@inbox.ru>
2021-05-03 10:39:46 +0300

    last

---
+++ src/com/content6/Field.java
@@ -0,0 +1,67 @@
+package com.content6;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+@SuppressWarnings("serial")
+public class Field extends JPanel {
+    private boolean paused;
+
+    private ArrayList<BouncingBall> balls = new ArrayList<BouncingBall>(10);
+    private final Timer repaintTimer = new Timer(10, new ActionListener() {
+        public void actionPerformed(ActionEvent ev) {
+            repaint();
+        }
+    });
+
+    public Field() {
+        setBackground(Color.WHITE);
+        repaintTimer.start();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D canvas = (Graphics2D) g;
+        for (BouncingBall ball : balls) {
+            ball.paint(canvas);
+        }
+    }
+
+    public void increase() {
+        for (BouncingBall ball : balls) {
+            ball.increaseSpeed();
+        }
+    }
+
+    public void reduce() {
+        for (BouncingBall ball : balls) {
+            ball.reduceSpeed();
+        }
+    }
+
+    public void addBall() {
+        balls.add(new BouncingBall(this));
+    }
+
+    public synchronized void pause() {
+        paused = true;
+    }
+
+    public synchronized void resume() {
+        paused = false;
+        notifyAll();
+    }
+
+    public synchronized void canMove(BouncingBall ball) throws
+            InterruptedException {
+        if (paused) {
+            wait();
+        }
+    }
+}
